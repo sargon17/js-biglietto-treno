1c3b6bdfd3ade783972a2955bcae10c672b3ce40
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-03-04 19:20:57 +0100

    working on logic

---
+++ script/script.js
@@ -0,0 +1,15 @@
+let searchBtn = document.querySelector("#search-btn");
+
+function calculatePrice(age, distance) {
+  if (age && distance) {
+    switch{
+        case
+    }
+  }
+}
+
+searchBtn.addEventListener("click", () => {
+  let userAge = document.querySelector("#age-input").value;
+  let userDistance = document.querySelector("#distance-input").value;
+  calculatePrice(userAge, userDistance);
+});
