db8c42518863deb1683aaa872ce977a8e1b9bbe7
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-03-05 18:57:28 +0100

    reordering js

--- script/script.js
+++ script/script.js
@@ -4,23 +4,31 @@ let ticketDistance = document.querySelector("#ticket-distance");
 let ticketPrice = document.querySelector("#ticket-price");
 
 let kmPrice = 0.21;
+let price;
+let discountedPrice;
+
+function validationInput(userAge, userDistance) {
+  //Data valaidation Function
+  if (userAge > 0 && userDistance >= 0) {
+    calculatePrice(userAge, userDistance); // if data are valid it passed them to calc Price function
+  } else {
+    console.log("age ore distance ar not valid", userAge, userDistance);
+  }
+}
 
 function calculatePrice(age, distance) {
-  let price;
-  let discountedPrice;
+  // Calculate the price of the ticket
+  price = 0;
+  discountedPrice = 0;
 
-  if (age > 0 && distance >= 0) {
-    if (age < 18) {
-      price = (distance * kmPrice).toFixed(2);
-      discountedPrice = (price * 0.8).toFixed(2);
-    } else if (age > 65) {
-      price = (distance * kmPrice).toFixed(2);
-      discountedPrice = (price * 0.6).toFixed(2);
-    } else {
-      price = (distance * kmPrice).toFixed(2);
-    }
+  if (age < 18) {
+    price = (distance * kmPrice).toFixed(2);
+    discountedPrice = (price * 0.8).toFixed(2);
+  } else if (age > 65) {
+    price = (distance * kmPrice).toFixed(2);
+    discountedPrice = (price * 0.6).toFixed(2);
   } else {
-    console.log("wrong age or distance input", age, distance);
+    price = (distance * kmPrice).toFixed(2);
   }
   displayTicket(price, discountedPrice, distance);
 }
@@ -40,5 +48,5 @@ function displayTicket(price, discountedPrice, distance) {
 searchBtn.addEventListener("click", () => {
   let userAge = document.querySelector("#age-input").value;
   let userDistance = document.querySelector("#distance-input").value;
-  calculatePrice(userAge, userDistance);
+  validationInput(userAge, userDistance);
 });
